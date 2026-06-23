468f5855bdaa7c51d30d46064cf34373e51db5f8
Bindu-Asundi <bindu.asundi@gmail.com>
2026-06-23 16:51:09 +0530

    First step: Let's Print Message
    Creating a Simple Java Program to Display a Message
    Writing a program that can disply a simple message for the user.

---
+++ PrintMessage.java
@@ -0,0 +1,8 @@
+public class Main {
+
+	public static void main(String[] args) {
+		//This is the instruction that tell the computer to print something on the screen.
+		System.out.println("Hello, World!");
+	}
+
+}
