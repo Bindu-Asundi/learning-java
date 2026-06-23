55594a293b664ed958f62eb1feafc94d24c5d043
Bindu-Asundi <bindu.asundi@gmail.com>
2026-06-23 16:53:50 +0530

    Let's Print Your Goal
    System.out.println():
    This is the instruction that tells the computer to print something on the screen.
    Whatever you write inside the double quotes(" ") will appear on the screen.

---
+++ PrintYourGoal.java
@@ -0,0 +1,9 @@
+
+public class PrintYourGoal {
+
+	public static void main(String[] args) {
+		//Whatever you write inside double quotes("") will appear on the screen.
+		System.out.println("My goal is to learn Java");
+	}
+
+}
