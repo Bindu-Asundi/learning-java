385282f68d75078e33c6c75f627be548cf3b05d6
Bindu-Asundi <bindu.asundi@gmail.com>
2026-06-23 16:56:49 +0530

    Printing Techniques: print() v/s println()
    print() prints output on the same line.
    println() prints output and moves to the next line.
    Use print() for continuous output and println() for line-by-line output.

---
+++ PrintAndPrintln.java
@@ -0,0 +1,21 @@
+
+public class PrintAndPrintln {
+
+	public static void main(String[] args) {
+		// Using println() prints output and moves to the next line.
+		System.out.println("Welcome to Sunrise Cafe:");
+		System.out.println("1. Pizza");
+		System.out.println("2. Burger");
+		System.out.println("3. Pasta");
+		
+		System.out.println(" ");
+		
+		
+		//Using print() prints output on the same line
+		System.out.print("Welcome to Sunrise Cafe: ");
+		System.out.print("1. Pizza ");
+		System.out.print("2. Burger ");
+		System.out.print("3. Pasta");
+	}
+
+}
